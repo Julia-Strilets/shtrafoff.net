2df283aa00a799b2d005fa8fec25f21cef63f8bc
Juliya Strilets <juliyastrilets@gmail.com>
2020-11-02 20:14:30 +0300

    header

--- styles/index.js
+++ styles/index.js
@@ -4,6 +4,10 @@ import './common/common.scss'
 
 // import your css here
 import './header/header.scss'
+import './logo/logo.scss'
 import './btn/btn.scss'
+import './nav/nav.scss'
 import './form/form.scss'
+import './auth/auth.scss'
+import './user-info/user-info.scss'
 import './footer/footer.scss'
